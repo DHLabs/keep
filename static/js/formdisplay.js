afbbbd4f8626cbecc96584c613ccc9f684a0c301
Patrick Wilson <patrick@patrick-Inspiron-7520.(none)>
2013-02-27 15:29:06 -0800

    Pushing the changes so questions display one at a time and the corresponding jquery script to do it

---
+++ static/js/formdisplay.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+        $(".control-group").hide();
+	$("#hide").click(function(){
+		$(".control-group").hide();
+	});
+	$("#show1").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(0)").show();
+	});
+	$("#show2").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(1)").show();
+	});
+	$("#show3").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(2)").show();
+	});
+	$("#show4").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(3)").show();
+	});
+	$("#show5").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(4)").show();
+	});
+	$("#show6").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(5)").show();
+	});
+	$("#show7").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(6)").show();
+	});
+	$("#show8").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(7)").show();
+	});
+	$("#show9").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(8)").show();
+	});
+	$("#show10").click(function(){
+		$(".control-group").hide();
+	        $(".control-group:eq(9)").show();
+	});
+
+});
