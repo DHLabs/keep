8b2cc2dbf8800e094c1df9067bf7e36defbcb975
Patrick Wilson <patrick@patrick-Inspiron-7520.(none)>
2013-03-21 23:06:57 -0700

    now submission of form redirects to the main page, as per rauls suggestion

--- static/js/xform_web.js
+++ static/js/xform_web.js
@@ -481,12 +481,7 @@
       });
       return $("#submit-xform").click(function() {
         alert("Thank you for your time!");
-	i = 0;
-	$(".control-group").hide();
-	$(".control-group").eq(i).show();
-	$("#prev").hide();
-	$("#next").show();
-	return $("#submit-xform").hide();
+	return window.location.replace("/");
       });
     });
   });
