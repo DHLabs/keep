88711f108829df6fe487aaad7963f81ae6a62549
Andrew Huynh <a5.t.huynh@gmail.com>
2013-04-03 16:23:47 -0700

    Added missing new repo form/template

---
+++ dhlab_backend/surveys/forms.py
@@ -0,0 +1,30 @@
+import re
+
+from django import forms
+from django.forms.util import ErrorList
+
+
+class NewRepoForm( forms.Form ):
+
+    name    = forms.CharField()
+
+    desc    = forms.CharField( widget=forms.Textarea, required=False )
+
+    privacy = forms.ChoiceField( choices=[ ('public', 'Public'),
+                                           ('private', 'Private') ],
+                                 widget=forms.RadioSelect )
+
+    xform_file      = forms.FileField()
+
+    def clean( self ):
+        if any( self.errors ):
+            return
+
+        # Ensure form name is a valid form
+        if re.search( '\W+', self.cleaned_data[ 'name' ] ) is not None:
+
+            errors = self._errors.setdefault( "name", ErrorList() )
+            errors.append( '''Repository name can not have
+                              spaces or special characters''' )
+
+        return self.cleaned_data
