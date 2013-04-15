3c0fa80bcdd5a493e7415a49566b4eb7524c534b
Andrew Huynh <a5.t.huynh@gmail.com>
2013-04-15 12:14:08 -0700

    Deploy script now checks to see if virtualenv has the latest dependencies

--- fabfile.py
+++ fabfile.py
@@ -38,6 +38,10 @@ def deploy():
         # Pull latest code from git
         run( 'git pull origin master' )
 
+        # Ensure we have the latest dependencies
+        run( 'workon dhlab-backend' )
+        run( 'pip install -r deps.txt' )
+
         # Start up all processes again
         run( 'supervisorctl start all' )
 
