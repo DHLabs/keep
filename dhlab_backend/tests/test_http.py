be4f51ad6c6624c883e87a5423b7e62c9161926c
Andrew Huynh <a5.t.huynh@gmail.com>
2013-05-01 15:15:36 -0700

    Added tests for repo creation

--- dhlab_backend/tests/test_http.py
+++ dhlab_backend/tests/test_http.py
@@ -3,6 +3,7 @@
     templating/etc errors on the pages.
 '''
 import googauth
+import os
 
 from base64 import b32encode
 
@@ -49,3 +50,51 @@ class HttpTests( HttpTestCase ):
 
         assert 'Registration Complete' in self.selenium.title
         assert len( mail.outbox ) == 1
+
+    def test_create_repo( self ):
+        '''
+            Tests the creation of a new repo
+        '''
+        self.test_login()
+
+        # Click on new repo button
+        new_repo = '/html/body/div[2]/div/div[2]/table/thead/tr/td/h4/div/a'
+        self.selenium.find_element_by_xpath( new_repo ).click()
+
+        # Fill out form
+        self.selenium.find_element_by_id( 'id_name' ).send_keys( 'test_repo' )
+
+        xform = os.path.abspath( '_data/test_docs/tutorial2.xls' )
+        self.selenium.find_element_by_id( 'id_xform_file' ).send_keys( xform )
+
+        submit = '/html/body/div[2]/div/div/div/form/div[2]/button'
+        self.selenium.find_element_by_xpath( submit ).click()
+
+        # Check that the new repo was created
+        repo_table = '/html/body/div[2]/div/div[2]/table/tbody/tr/td[2]/a'
+        repos = self.selenium.find_elements_by_xpath( repo_table )
+        assert any( [ x.text == 'test_repo' for x in repos ] )
+
+    def test_create_repo_existing( self ):
+        '''
+            Tests creation of a new repo with an existing name.
+        '''
+        self.test_login()
+
+        # Click on new repo button
+        new_repo = '/html/body/div[2]/div/div[2]/table/thead/tr/td/h4/div/a'
+        self.selenium.find_element_by_xpath( new_repo ).click()
+
+        # Fill out form
+        self.selenium.find_element_by_id( 'id_name' ).send_keys( 'tutorial' )
+
+        xform = os.path.abspath( '_data/test_docs/tutorial2.xls' )
+        self.selenium.find_element_by_id( 'id_xform_file' ).send_keys( xform )
+
+        submit = '/html/body/div[2]/div/div/div/form/div[2]/button'
+        self.selenium.find_element_by_xpath( submit ).click()
+
+        # Check that we correctly report an error
+        error = '/html/body/div[2]/div/div/div/form/ul/li'
+        elem = self.selenium.find_element_by_xpath( error )
+        assert elem.text == 'Repository already exists with this name'
