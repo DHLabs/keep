6e06775a6cdf8c146bde1a6547da37bb6cb0e7f0
Andrew Huynh <a5.t.huynh@gmail.com>
2013-05-09 12:03:44 -0700

    Updated settings to use CloudFront CDN

--- dhlab_backend/settings/production.py
+++ dhlab_backend/settings/production.py
@@ -11,6 +11,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID       = AWS[ 'ACCESS_KEY_ID' ]
 AWS_SECRET_ACCESS_KEY   = AWS[ 'SECRET_KEY' ]
 AWS_STORAGE_BUCKET_NAME = 'keep-static'
+# Use Amazon Cloudfront
+AWS_S3_CUSTOM_DOMAIN    = '//d1sht9pkbliybl.cloudfront.net'
 AWS_HEADERS = {
     'Expires': 'Thu, 15 Apr 2030 20:00:00 GMT',
     'Cache-Control': 'max-age=86400',
