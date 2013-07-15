664237d8b3c4d246fa73014fb1320352a4ffa521
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2013-07-15 10:56:01 -0700

    More added to api - filter

--- dhlab_backend/vocab/api.py
+++ dhlab_backend/vocab/api.py
@@ -1,23 +1,31 @@
 import pymongo
 
 from backend.db import MongoDBResource
-from backend.serializers import CSVSerializer
 
 from tastypie import fields
 from tastypie.resources import ModelResource
 
 class VocabResource( MongoDBResource ):
 	'''
-		VocabResource are used to access the database,
+		VocabResource is used to access the database,
 		And query for autocomplete purposes.
 	'''
+
 	id		= fields.CharField( attribute='_id' )
 	data 	= fields.CharField( attribute='data', null=True )
 
 	class Meta:
+		queryset = Vocab.objects.all()
+
 		collection = 'vocab'
 		resource_name = 'vocab'
-		object_class = Document
-		serializer = CSVSerializer()
 
-	def get_detail( self, request, **kwargs )
+		list_allowed_methods = [ 'get' ]
+
+		filtering = {
+			'data' : ( 'icontains', )
+		}
+
+	def build_filters ( self, filters=None ):
+		if 'data__icontains' in filters:
+			return super( VocabResource, self ).build_filters( filters )
