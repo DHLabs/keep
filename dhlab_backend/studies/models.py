36afbeedc87d404fd0f02bcb53cbc473617734fd
Andrew Huynh <a5.t.huynh@gmail.com>
2013-08-28 13:45:52 -0700

    Added a study serializer

--- dhlab_backend/studies/models.py
+++ dhlab_backend/studies/models.py
@@ -1,4 +1,24 @@
+from django.core.serializers.python import Serializer
 from django.db import models
+
+
+class StudySerializer( Serializer ):
+    '''
+        Converts a QuerySet of Study objects into a specific JSON format.
+    '''
+    def end_object( self, obj ):
+        # Convert timestamps into JSON timestamps
+        self._current[ 'date_updated' ] = obj.date_updated.strftime( '%Y-%m-%dT%X' )
+        self._current[ 'date_created' ] = obj.date_created.strftime( '%Y-%m-%dT%X' )
+
+        self._current[ 'id' ] = obj.id
+
+        self._current.pop( 'user' )
+        self._current.pop( 'org' )
+
+        print self._current
+
+        self.objects.append( self._current )
 
 
 class Study( models.Model ):
