cfada2c5a86de73ad10f6bafd08ce21d7c8d318a
Andrew Huynh <a5.t.huynh@gmail.com>
2013-09-09 14:03:57 -0700

    Remove unnecessary imports from geocoord conversion script and added some nice documentation

--- dhlab_backend/repos/management/commands/convert_geocoords.py
+++ dhlab_backend/repos/management/commands/convert_geocoords.py
@@ -1,15 +1,25 @@
 from bson import ObjectId
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
 
 from backend.db import db
-from repos.models import Repository
 
 
 class Command( BaseCommand ):
     help = 'Go through all data in '
 
     def _flatten( self, root ):
+        '''
+            Flattens a repository field list into an array of fields. We also
+            filter for specifically "geopoint" field types.
+
+            Params
+            ------
+            root : list
+                Root represents a list of "fields". If the field type is a group,
+                we jump deeper until we reach the end of that group. Otherwise,
+                we filter for geopoint fields and only add those to our list
+                of fields.
+        '''
         fields = []
 
         for field in root:
@@ -22,18 +32,30 @@ class Command( BaseCommand ):
         return fields
 
     def handle( self, *args, **options ):
+        '''
+            Filter through *all* data elements in the MongoDB database. This
+            may take a while depending on the number of elements in the database.
 
+            We search for geopoint fields and convert them into the more useful
+            "geocoord" dictionary which separates out lat/lng/altitude into
+            useful attributes that can be search/filtered/etc.
+        '''
         for repo in db.repo.find():
 
             geofields = self._flatten( repo.get( 'fields' ) )
 
+            # If this repository has no geopoint fields, continue onto the
+            # next data repo.
             if len( geofields ) == 0:
                 continue
 
+            # Filter out for all data elements for this repository.
             for item in db.data.find( { 'repo': ObjectId( repo.get( '_id' ) ) } ):
 
                 data = item.get( 'data' )
 
+                # Loop through each geofield and convert into our geodata object
+                # when possible.
                 for gfield in geofields:
                     field_name = gfield.get( 'name' )
 
@@ -47,21 +69,33 @@ class Command( BaseCommand ):
 
                         geostring = data[ field_name ]
 
+                        # If for some reason, this item was already generated
+                        # skip it. ( did we run this twice? )
                         if not isinstance( geostring, basestring ):
+                            print 'Already converted... did you run this twice?'
                             continue
 
                         geostring = geostring.split( ' ' )
 
                         if len( geostring ) == 4:
-                            geodata[ 'lat' ] = float( geostring[0] )
-                            geodata[ 'lng' ] = float( geostring[1] )
-                            geodata[ 'altitude' ] = float( geostring[2] )
-                            geodata[ 'accuracy' ] = float( geostring[3] )
+                            # Attempt to convert location string into the geodata
+                            # object.
+                            try:
+                                geodata[ 'lat' ] = float( geostring[0] )
+                                geodata[ 'lng' ] = float( geostring[1] )
+                                geodata[ 'altitude' ] = float( geostring[2] )
+                                geodata[ 'accuracy' ] = float( geostring[3] )
+                            except ValueError:
+                                # If for some reason we can't convert this string
+                                # save the string as a "comment" metadata in the
+                                # new geodata object
+                                geodata[ 'comment' ] = data[ field_name ]
                         else:
                             geodata[ 'comment' ] = data[ field_name ]
 
-                        full_field = 'data.%s' % ( field_name )
-
+                        # Finally go in and update the said field in the actual
+                        # database!
                         print 'Updating %s' % ( item.get( '_id' ) )
+                        full_field = 'data.%s' % ( field_name )
                         db.data.update( { '_id': item.get( '_id' ) },
                                         { '$set': { full_field: geodata } } )
