f272e5223a431d3d62817aeebe36af618c5c2755
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2013-09-30 14:56:01 -0700

    Rework of xls conversion, now returning something, just not right something

--- dhlab_backend/openrosa/json_xls_convert.py
+++ dhlab_backend/openrosa/json_xls_convert.py
@@ -1,32 +1,36 @@
 import re
 
+from django.http import HttpResponse
 from openpyxl import Workbook
 
+from openpyxl.writer.excel import save_virtual_workbook
+
 class jsonXlsConvert():
-	#def __init__(self): 
-	#	pass
-
-
-	def writeToXls(self, json_dict, file_name):
-		wb = Workbook()
-		worksheet1 = wb.get_active_sheet()
-		worksheet1.title = 'survey'
-		surveyHeaders = ['type', 'name', 'label', 'hint', 'media::image',
+	def __init__(self, file_name): 
+		jsonXlsConvert.wb = Workbook()
+		jsonXlsConvert.worksheet1 = self.wb.get_active_sheet()
+		self.worksheet1.title = 'survey'
+		jsonXlsConvert.surveyHeaders = ['type', 'name', 'label', 'hint', 'media::image',
 						 'media::video', 'media::audio', 'constraint', 
 						 'constraint_message', 'required', 'default', 
 						 'relevant', 'read_only', 'calculation', 'appearance']
-		self.writeArrayToXLS(worksheet1, surveyHeaders, 'A1')
+		self.writeArrayToXLS(self.worksheet1, self.surveyHeaders, 'A1')
 
-		worksheet2 = wb.create_sheet(1)
-		worksheet2.title = 'choices'
-		choicesHeaders = ['list name', 'name', 'label', 'image']
-		self.writeArrayToXLS(worksheet2, choicesHeaders, 'A1')
+		jsonXlsConvert.worksheet2 = self.wb.create_sheet(1)
+		self.worksheet2.title = 'choices'
+		jsonXlsConvert.choicesHeaders = ['list name', 'name', 'label', 'image']
+		self.writeArrayToXLS(self.worksheet2, self.choicesHeaders, 'A1')
+		jsonXlsConvert.file_name = (file_name + ".xls")
 
-		self.recurseWriter(json_dict, worksheet1, worksheet2, surveyHeaders, choicesHeaders)
+	def writeToXls(self, json_dict):
 
-		return wb.save(file_name + '.xls')
+		self.recurseWriter(json_dict)
 
-	def recurseWriter(self, json_dict, wb1, wb2, wb1Headers, wb2Headers):
+		response = HttpResponse(save_virtual_workbook(self.wb), content_type='application/vnd.ms-excel')
+		response['Content-Disposition'] = 'attachment; filename="' + self.file_name + '"'
+		return response
+
+	def recurseWriter(self, json_dict):
 		iterRow = 1
 		choicesRow = 1
 		for element in json_dict:
@@ -35,31 +39,31 @@ class jsonXlsConvert():
 
 			if element.get("type") == 'group':
 				surveyWriteRow = ['begin group']
-				self.writeArrayToXLS(wb1, surveyWriteRow, ('A' + `iterRow`))
-				self.recurseWriter(element.get("children"), wb1, wb2, wb1Headers, wb2Headers)
+				self.writeArrayToXLS(self.worksheet1, surveyWriteRow, ('A' + `iterRow`))
+				self.recurseWriter(element.get("children"))
 				iterRow += 1
 				surveyWriteRow = ['end group']
-				self.writeArrayToXLS(wb1, surveyWriteRow, ('A' + `iterRow`))
+				self.writeArrayToXLS(self.worksheet1, surveyWriteRow, ('A' + `iterRow`))
 
 			else:
-				for header in wb1Headers:
+				for header in self.surveyHeaders:
 					if header in element:
 						surveyWriteRow += [element.get(header)]
 					else:
 						surveyWriteRow += [""]
-				self.writeArrayToXLS(wb1, surveyWriteRow, ('A' + `iterRow`))
+				self.writeArrayToXLS(self.worksheet1, surveyWriteRow, ('A' + `iterRow`))
 
 			if 'choices' in element:
 				choiceOptions = element['choices']
 				choicesRow += 1
 				choiceWriteRow = []
 				for choice in choiceOptions:
-					for header in wb2Headers:
+					for header in self.choicesHeaders:
 						if header in choice:
 							choiceWriteRow += [choice.get(header)]
 						else:
 							choiceWriteRow += [""]
-					self.writeArrayToXLS(wb2, choiceWriteRow, ('A' + `choicesRow`))
+					self.writeArrayToXLS(self.worksheet2, choiceWriteRow, ('A' + `choicesRow`))
 					choiceWriteRow = []
 
 	'''Utility function to write an array to excel using openpyxl'''
@@ -81,4 +85,3 @@ class jsonXlsConvert():
 				cell_str = '%s%d' % (chr(col_ind + 64 ), int(row) + i)
 				worksheet.cell(cell_str).value = array_to_write[i]
 				# print cell_str
-
