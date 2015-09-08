1e82a68b100a40296b722c692d5204178bc51756
Wes Vetter <wes.vetter@gmail.com>
2015-09-08 10:38:34 -0700

    Delete cruft in openrosa subapp

--- keep_backend/openrosa/urls.py
+++ keep_backend/openrosa/urls.py
@@ -10,30 +10,11 @@
 '''
 from django.conf.urls import patterns, url
 
-
 urlpatterns = patterns( 'openrosa.views',
 
     # Return a list of forms uploaded/shared to the specified user
     url( r'^bs/(?P<username>\w+)/formList$', 'formlist' ),
 
+    # Parse an XML submission file and add the data into the specified repo.
     url( r'^bs/(?P<username>\w+)/submission', 'xml_submission'),
-
-    # Returns a list of submissions for a specified form & user
-    url( r'^bs/(?P<username>\w+)/forms/(?P<form_id>[^/]+)/submission/(?P<sub_id>[^/]+)$',
-         'submission_detail' ),
-
 )
-
-# # odk data urls
-# url(r"^submission$", 'odk_logger.views.submission'),
-# url(r"^(?P<username>\w+)/formList$", 'odk_logger.views.formList'),
-# url(r"^(?P<username>\w+)/xformsManifest/(?P<id_string>[^/]+)$",
-#     'odk_logger.views.xformsManifest'),
-# url(r"^(?P<username>\w+)/submission$", 'odk_logger.views.submission'),
-# url(r"^(?P<username>\w+)/bulk-submission$", 'odk_logger.views.bulksubmission'),
-# url(r"^(?P<username>\w+)/bulk-submission-form$", 'odk_logger.views.bulksubmission_form'),
-# url(r"^(?P<username>\w+)/forms/(?P<id_string>[^/]+)/form\.xml$", 'odk_logger.views.download_xform', name="download_xform"),
-# url(r"^(?P<username>\w+)/forms/(?P<id_string>[^/]+)/form\.xls$", 'odk_logger.views.download_xlsform', name="download_xlsform"),
-# url(r"^(?P<username>\w+)/forms/(?P<id_string>[^/]+)/form\.json", 'odk_logger.views.download_jsonform', name="download_jsonform"),
-# url(r"^(?P<username>\w+)/delete/(?P<id_string>[^/]+)/$", 'odk_logger.views.delete_xform'),
-# url(r"^(?P<username>\w+)/(?P<id_string>[^/]+)/toggle_downloadable/$", 'odk_logger.views.toggle_downloadable'),
